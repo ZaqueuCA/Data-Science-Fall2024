e6118ded86c3c9144b0cff6159a84420a62ccd46
ZaqueuCA <zantonio@claflin.edu>
2024-09-25 16:45:11 +0200

    Add files via upload

--- ANOVA-analysis.R
+++ ANOVA-analysis.R
@@ -19,3 +19,15 @@ PATH <- "https://raw.githubusercontent.com/guru99-edu/R-Programming/master/poiso
 df <- read.csv(PATH)
 
 head(df)
+
+#Applying ANOVA test to columns time (dependent) and poison (independent)
+
+anova_one_way <- aov(time~poison, data = df) 
+
+summary(anova_one_way)
+
+            Df Sum Sq Mean Sq F value   Pr(>F)    
+poison       1 0.9316  0.9316   20.67 3.96e-05 ***
+Residuals   46 2.0735  0.0451                     
+---
+Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
